d4c21b475ba0a99971a68037dcae35d1ed5b18ec
Waldemar Asmus <waldemar.asmus@gus-group.com>
2025-03-24 12:34:36 +0100

    now working

--- agm-directions-sample/src/main.ts
+++ agm-directions-sample/src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import '@angular/compiler'; // Add this line to include the JIT compiler
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
